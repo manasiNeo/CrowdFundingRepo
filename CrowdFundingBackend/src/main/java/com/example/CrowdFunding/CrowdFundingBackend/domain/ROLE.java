c10157ea5ad2fa3bc7e639574475f41068578637
Manasi Yadav <163994607+manasiNeo@users.noreply.github.com>
2024-05-07 09:46:39 +0530

    Delete CrowdFundingBackend directory

--- CrowdFundingBackend/src/main/java/com/example/CrowdFunding/CrowdFundingBackend/domain/ROLE.java
+++
@@ -1,9 +0,0 @@
-package com.example.CrowdFunding.CrowdFundingBackend.domain;
-
-public enum ROLE {
-    
-    //ROLE_CAMPAIGN,
-    ROLE_ADMIN,
-    ROLE_USER
-    //ROLE_PAYMENT
-}
--- CrowdFundingBackend/src/main/java/com/example/CrowdFunding/CrowdFundingBackend/exception/UserException.java
+++
@@ -1,9 +0,0 @@
-package com.example.CrowdFunding.CrowdFundingBackend.exception;
-
-public class UserException extends Exception{
-    
-    public UserException(String message) {
-		super(message);
-	}
-
-}
